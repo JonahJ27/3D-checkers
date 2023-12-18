562973aa60caa78fc22f8c8fd9b5306332f98561
JonahJ27 <jonah.jaffe@guardian.ag>
2023-12-18 13:56:03 -0500

    fixed logic for captures and blocking double moves

--- src/board.rs
+++ src/board.rs
@@ -21,17 +21,19 @@ impl BoardState{
     pub fn try_to_move(& mut self, mut position_list: Vec<String>, red_turn: bool) -> Result<String, String> {
         let mut taken_piece_posns = Vec::new();
         let first_pos = position_list.pop().ok_or("Not enough positions entered for a move.")?;
-
+        let mut moved_yet = false;
         let mut curr_col = pos_to_col(first_pos.clone())?;
         let mut curr_row = pos_to_row(first_pos.clone())?;
-        println!("row: {}, col: {}", curr_row, curr_col);
         // need checking for length one lists
         while position_list.len() > 0 {
+            if moved_yet && taken_piece_posns.len() == 0 {
+                break;
+            }
             let next_pos:String = position_list.pop().ok_or(
                 "Not enough positions entered for a move.")?;
             let next_col = pos_to_col(next_pos.clone())?;
             let next_row = pos_to_row(next_pos.clone())?; 
-            match self.is_valid_move(curr_row, curr_col, next_row, next_col, red_turn) {
+            match self.is_valid_move(curr_row, curr_col, next_row, next_col, red_turn, moved_yet) {
                 Ok(valid) => if valid {
                     if let Some(posn) = self.get_taken_piece(curr_row, curr_col, next_row, next_col) {
                         taken_piece_posns.push(posn);
@@ -44,10 +46,19 @@ impl BoardState{
             }
             curr_row = next_row;
             curr_col = next_col;
+            moved_yet = true;
         }
+        if position_list.len() > 0 {
+            return Err("Cannot move 1 space after any other move.".to_owned());
+        }
+
         let first_pos_type = self.data[pos_to_row(first_pos.clone())? as usize][pos_to_col(first_pos.clone())? as usize];
         self.data[pos_to_row(first_pos.clone())? as usize][pos_to_col(first_pos.clone())? as usize] = Tile::Empty;
         self.data[curr_row as usize][curr_col as usize] = first_pos_type;
+
+        for pos in taken_piece_posns {
+            self.data[pos.0 as usize][pos.1 as usize] = Tile::Empty;
+        }
 
         
         // add removal of taken pieces
@@ -67,13 +78,17 @@ impl BoardState{
     } 
 
     fn is_valid_move(&self, curr_row: u32, curr_col: u32, next_row: u32, 
-                     next_col: u32, red_turn: bool) -> Result<bool, String> {
+                     next_col: u32, red_turn: bool, moved_yet: bool) -> Result<bool, String> {
         let curr_tile = self.get_tile_at(curr_row, curr_col)?;
         let next_tile = self.get_tile_at(next_row, next_col)?;
         let mut is_royal = false;    
         if red_turn {
             match curr_tile {
-                Tile::Empty => return Err("Target position".to_owned() + " is empty."),
+                Tile::Empty => {
+                    if !moved_yet {
+                        return Err("Target position".to_owned() + " is empty.")
+                    }
+                },
                 Tile::Black => return Err("Target position".to_owned() + " is black."),
                 Tile::Red => is_royal = false,
                 Tile::BlackRoyals => return Err("Target position".to_owned() + " is black royal."),
@@ -82,7 +97,11 @@ impl BoardState{
         }
         else {
             match curr_tile {
-                Tile::Empty => return Err("Target position".to_owned() + " is empty."),
+                Tile::Empty => {
+                    if !moved_yet {
+                        return Err("Target position".to_owned() + " is empty.")
+                    }
+                },
                 Tile::Black => is_royal = false,
                 Tile::Red => return Err("Target position".to_owned() + " is red."),
                 Tile::BlackRoyals => is_royal = true,
@@ -114,28 +133,28 @@ impl BoardState{
                 if (curr_row as i32 - next_row as i32) == -2 &&
                    (curr_col as i32 - next_col as i32).abs() == 2 {
                     let tile_to_take = self.get_tile_at(
-                    (curr_row as i32 + (curr_row as i32 - next_row as i32)) as u32,
-                    (curr_col as i32 + (curr_col as i32 - next_col as i32)) as u32)?;
+                    (curr_row as i32 - (curr_row as i32 - next_row as i32) / 2) as u32,
+                    (curr_col as i32 - (curr_col as i32 - next_col as i32) / 2) as u32)?;
 
                     return Ok(tile_to_take == &Tile::Black || tile_to_take == &Tile::BlackRoyals);
                 }
                 else {
                     return Ok((curr_row as i32 - next_row as i32) == -1 &&
-                            (curr_col as i32 - next_col as i32).abs() == 1)
+                            (curr_col as i32 - next_col as i32).abs() == 1 && !moved_yet) 
                 }
             }
             else {
                 if (curr_row as i32 - next_row as i32) == 2 &&
                    (curr_col as i32 - next_col as i32).abs() == 2 {
                     let tile_to_take = self.get_tile_at(
-                    (curr_row as i32 + (curr_row as i32 - next_row as i32)) as u32,
-                    (curr_col as i32 + (curr_col as i32 - next_col as i32)) as u32)?;
+                    (curr_row as i32 - (curr_row as i32 - next_row as i32) / 2) as u32,
+                    (curr_col as i32 - (curr_col as i32 - next_col as i32) / 2) as u32)?;
 
                     return Ok(tile_to_take == &Tile::Red || tile_to_take == &Tile::RedRoyals);
                 }
                 else {
                     return Ok((curr_row as i32 - next_row as i32) == 1 &&
-                            (curr_col as i32 - next_col as i32).abs() == 1)
+                            (curr_col as i32 - next_col as i32).abs() == 1 && !moved_yet)
                 }
             }
         }
@@ -145,8 +164,8 @@ impl BoardState{
         next_col: u32) -> Option<(u32, u32)> {
         if (curr_row as i32 - next_row as i32).abs() == 2 && 
                (curr_col as i32 - next_col as i32).abs() == 2 {
-            return Some(((curr_row as i32 + (curr_row as i32 - next_row as i32)) as u32,
-                    (curr_col as i32 + (curr_col as i32 - next_col as i32)) as u32))        
+            return Some(((curr_row as i32 - (curr_row as i32 - next_row as i32) / 2) as u32,
+                    (curr_col as i32 - (curr_col as i32 - next_col as i32) / 2) as u32))        
         }
         return None;
     }
